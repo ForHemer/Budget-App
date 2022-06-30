eb82d24341bafe160afc46f7225bdff773965801
ForHemer <foreste.hemerson@gmail.com>
2022-06-30 01:08:34 -0400

    Add a new transaction page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
-  resources :expenses
-  resources :groups
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users 
+  devise_scope  :user do
+    get 'users/sign_out' => 'devise/sessions#destroy'
+  end
 
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users do 
+    resources :groups do
+      resources :expenses
+    end
+  end
   # Defines the root path route ("/")
-  # root "articles#index"
+   root "splash#index"
 end
